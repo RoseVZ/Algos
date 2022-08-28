d12b01ac7fc7503233ca21ac57298b63e3554572
RoseVZ <priyarosev949@gmail.com>
2022-08-28 16:50:14 +0530

    comments on knapsack

--- greedy/fractional_knapsack.cpp
+++ greedy/fractional_knapsack.cpp
@@ -10,7 +10,7 @@ struct Item
     int value;
     int weight;
 };
-
+    //comparator function
     bool cmp(struct Item a, struct Item b)
     {
         double w1=(a.value*1.0)/(a.weight*1.0);
@@ -19,20 +19,23 @@ struct Item
     }
     double fractionalKnapsack(int W, Item arr[], int n)
     {
-        // Your code here
+        // sort the array on basis of the unit price(descending)
         sort(arr,arr+n,cmp);
         double current_weight=0.0;
         double profit=0.0;
         
         for(int i=0;i<n;i++)
         {
+            //if the capacity is crossed
             if(current_weight+arr[i].weight<=W)
             {
                 profit+=arr[i].value;
                 current_weight+=arr[i].weight;
             }
             else{
+                //find the fraction of item to be added ie diff
                 int diff=(W-current_weight);
+                //calculate profit by multiplying the fraction and unit price per weight
                 profit+=diff*(arr[i].value*1.0/arr[i].weight*1.0);
                 
                 break;
